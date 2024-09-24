16c03380cb89c5aff05f3419759f671e452a5a58
MTDMichi <michael.gabel@wuerth-industrie.com>
2024-09-24 22:14:47 +0200

    Klasse commune erstellt
    habe versucht Gabi nachzumachen

--- wgRechner-rest-main/src/main/java/mosbach/dhbw/de/wgrechner/model/Commune.java
+++ wgRechner-rest-main/src/main/java/mosbach/dhbw/de/wgrechner/model/Commune.java
@@ -1,4 +1,21 @@
 package mosbach.dhbw.de.wgrechner.model;
 //WIP Noch keinen Nutzen
 public class Commune {
+
+    private static int communeIdCounter; //Zeigt an wieviele WGs existieren
+
+    private String communeId;
+    
+    private int maxPeopleNumber;
+    private String location;
+
+    public Commune(String communeId, int maxPeopleNumber, String location){
+        this.communeId = commune.createCommuneID(),
+    }
+
+    private static synchronized String createCommuneID(){
+        return "commune"+ (++Commune.communeIdCounter);
+    }
+
+
 }
